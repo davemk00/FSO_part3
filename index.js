ad6b31e5d2a6b6965a866860713134bd3bd69497
davemk00 <david.king@zoho.com>
2020-02-21 22:48:58 +0000

    test1

--- index.js
+++ index.js
@@ -12,6 +12,53 @@ app.use(cors())
 // bodyParser middleware
 app.use(bodyParser.json())
 
+// Logger middleware when not a POST request
+// Use default 'tiny' string format
+app.use(morgan('tiny', { 
+  skip: (req) => { return req.method == "POST" },
+  stream: process.stdout 
+}));
+
+// Logger when it is a POST request
+// Create new token
+morgan.token('reqData', (req) => {
+  return(JSON.stringify({ "name": req.body.name, "number": req.body.number}))
+});
+
+// set string format
+var POSTLoggerFormat = ':method :url :status :res[content-length] - :response-time ms :reqData';
+
+// log POST using string format and created token
+app.use(morgan(POSTLoggerFormat, {
+  skip: (req) => { return req.method != "POST" },
+  stream: process.stdout 
+}));
+
+
+
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
 
 app.get('/', (req, res) =>{
   res.send(`<h1>DK Phonebook</h1>
@@ -23,8 +70,73 @@ app.get('/', (req, res) =>{
   </ul></p>`)
 })
 
+app.get('/info', (req, res) =>{
+  res.send(
+    `<p>The phonebook has info for ${persons.length} people</p>
+    <p>${Date().toLocaleString()}</p>`)
+}) 
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+  
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'content missing'
+    })
+  }
+
+  //Check that the person is already in the list 
+  const checkExists = persons.findIndex(person => person.name.toLowerCase() === body.name.toLowerCase())
+
+  if (checkExists > 0) {
+    console.log("error: name already exists")
+    return res.status(409).json({
+      error: 'name already exists'
+    })
+  }
+
+
+  const person = {
+    name: body.name,
+    number: body.number || false,
+    id: getRandomInt(10000),
+  }
+
+  persons = persons.concat(person)
+  res.json(body)
+})
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});

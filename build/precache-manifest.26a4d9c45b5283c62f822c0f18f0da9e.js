1d0fe9f38edcec3e0aac2efb444311caa52da666
davemk00 <david.king@zoho.com>
2020-02-22 00:36:50 +0000

    uibuild

---
+++ build/precache-manifest.26a4d9c45b5283c62f822c0f18f0da9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ac9a824be3d38c8d490beb83c5282e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7faa39041ad05b06b0e",
+    "url": "/static/css/main.bf4c6b40.chunk.css"
+  },
+  {
+    "revision": "0f07f5e4ad31d4b19121",
+    "url": "/static/js/2.684852d2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.684852d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7faa39041ad05b06b0e",
+    "url": "/static/js/main.6703cdd7.chunk.js"
+  },
+  {
+    "revision": "31bfc067a06b18f0c566",
+    "url": "/static/js/runtime-main.bbc88e4f.js"
+  }
+]);

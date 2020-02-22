981596cc7b52d099acd30c86e2acb7ed4be14ae3
David K <david.king@zoho.com>
2020-02-22 19:21:38 +0000

    env not working

--- models/person.js
+++ models/person.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
+console.log(process.env.MONGODB_URI)
+console.log(process.env.PORT)
 
 mongoose.connect(url, { useNewUrlParser: true })
   .then(result => {    
